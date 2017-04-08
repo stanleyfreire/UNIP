7cd4470e770976630fa9efc72dbd9c4886bb5266
stanleyfreire <stanley_freire@hotmail.com>
2017-04-08 10:08:41 -0300

    Add project files.

---
+++ HelloWorld/HelloWorld/PageForm.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class PageForm : ContentPage
+	{
+		public PageForm ()
+		{
+			InitializeComponent ();
+            slider.Value = 0.5;
+		}
+	}
+}
---
+++ HelloWorld/HelloWorld/QuotesPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QuotesPage : ContentPage
+    {
+        private int index = 0;
+        private string[] quotes = new string[]
+        {
+            "More Life",
+            "More Everything",
+            "Once More.."
+        };
+
+        public QuotesPage()
+        {
+            InitializeComponent();
+            currentQuotes.Text = quotes[index];
+            
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            index++;
+            if (index >= quotes.Length)
+                index = 0;
+
+            currentQuotes.Text = quotes[index];
+        }
+    }
+}
